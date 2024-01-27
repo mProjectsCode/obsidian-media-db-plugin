9257d8e7227ccf8cea148db538e305cd7120fb32
mProjectsCode <m.projects.code@gmail.com>
2024-01-27 21:50:20 +0100

    update packages; run formatter

--- src/settings/Settings.ts
+++ src/settings/Settings.ts
@@ -162,7 +162,7 @@ export class MediaDbSettingTab extends PluginSettingTab {
 					});
 			});
 
-			new Setting(containerEl)
+		new Setting(containerEl)
 			.setName('Moby Games key')
 			.setDesc('API key for "www.mobygames.com".')
 			.addText(cb => {
